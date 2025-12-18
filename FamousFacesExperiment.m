1b2ebd975d2ead2854f5229c4d2dcd1c0b374c13
Jan <jan.brandstaeter@stud.plus.ac.at>
2025-12-18 16:12:08 +0100

    Testcommit weil ichs nicht raff

--- FamousFacesExperiment.m
+++ FamousFacesExperiment.m
@@ -4,3 +4,4 @@ myBackgroundColour = [255 255 255];
 myWindow = Screen('OpenWindow',myScreen, myBackgroundColour, [0 0 1000 840]);
 KbWait;
 Screen('CloseAll');
+%ich kann nicht mehr ich will nicht mehr ich halte das alles nicht mehr aus
